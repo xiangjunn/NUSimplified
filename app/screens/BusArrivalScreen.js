2b809cd458b163ed764b0c27f0376e677dc8b7a4
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2021-07-08 23:44:17 +0800

    Show distance from bus stop based on current location

--- app/screens/BusArrivalScreen.js
+++ app/screens/BusArrivalScreen.js
@@ -1,12 +1,16 @@
-import { Container, Text, Icon, Form, Header, Item, Input } from 'native-base';
+import { Container, Text, Icon, Form, Header, Item, Input, Button } from 'native-base';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, TouchableOpacity, ActivityIndicator, FlatList } from 'react-native';
 import { firebase } from '../../firebase';
 import BusTimings from './BusTimings';
-import { searchFilter } from '../backend/functions'
+import { searchFilter } from '../backend/functions';
+import * as Location from 'expo-location';
+import { getDistance } from 'geolib';
 
 function BusArrivalScreen() {
     const [loading, setLoading] = useState(true);
+    const [currentCoordinates, setCurrentCoordinates] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
     const [busStops, setBusStops] = useState([]);
     const [expand, setExpand] = useState({"selected": null});
     const [arrayholder, setArrayholder] = useState([]);
@@ -25,17 +29,40 @@ function BusArrivalScreen() {
           });
           setArrayholder(arr)
           setBusStops(arr);
-          setLoading(false);
         });
+
+        (async () => {
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+          }
+    
+          await Location.watchPositionAsync({timeInterval: 5000, distanceInterval: 10}, setCoordinates)
+          setLoading(false);
+        })();
     
       // Unsubscribe from events when no longer in use
       return () => subscriber();
     }, []);
 
+    function setCoordinates(location) {
+      const coordinates = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      }
+      setCurrentCoordinates(coordinates);
+    }
+
+    // coordinates consist of 2 keys namely latitude and longitude
+    function findDistance(firstCoordinates, secondCoordinates) {
+      const distance = getDistance(firstCoordinates, secondCoordinates);
+      return distance + "m";
+    }
+
     if (loading) {
       return <ActivityIndicator animating={true} size="large" style={{opacity:1}} color="#999999" />
     }
-
 
     return (
       <Container>
@@ -62,7 +89,11 @@ function BusArrivalScreen() {
           setExpand({"selected": item.key});
         }}>
           <Form style={{flex: 1, marginVertical: 5, marginHorizontal: 10}}>
+            <Form flexDirection='row'>
           <Text style={{fontWeight: 'bold'}}>{item.name}</Text>
+          <Text style={styles.distance}>
+            {currentCoordinates ? findDistance(currentCoordinates, currentCoordinates): ""}</Text>
+          </Form>
           <Text>{item.key}</Text>
           </Form>
         </TouchableOpacity>
@@ -125,6 +156,14 @@ const styles = StyleSheet.create({
     height: 65,
     flex: 1,
     flexDirection: "row",
+  },
+  distance: {
+    paddingHorizontal: 5,
+    marginLeft: 5,
+    borderRadius: 5,
+    borderWidth: 1,
+    color: 'blue',
+    fontWeight: 'bold'
   }
 });
 
